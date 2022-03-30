16a851ed4fddee2346660b3292ef155063d7bdc6
Andrew <andrewmgunn26@gmail.com>
2022-03-29 21:15:25 -0500

    more fixes

--- demo.js
+++ demo.js
@@ -12,41 +12,25 @@ $(function () {
             // params
             // success callback
             // datatype returned
-            'http://itunes.apple.com/search?term=' + term2 + '&limit=' + limit,
-            {},
+            //'http://itunes.apple.com/search?term=' + term2 + '&limit=' + limit,
+            'http://itunes.apple.com/search?term=' + term2,
+            {
+                limit: $('#maxLimit').val()
+            },
             function (data) {
-                console.log(data);
 
                 for (i in data.results) {
 
-                    let desc = '';
-                    if (data.results[i].shortDescription === undefined) {
-                        desc = 'un';
-                    } else {
-                        desc = data.results[i].shortDescription;
-                    }
 
-
-                    $('#dataTable').append(
-                        `
-<tr>
-<th scope="row">${num++}</th>
-                <td><img src="${data.results[i].artworkUrl60}" /></td>
-
-
-                <td>${data.results[i].artistName  === undefined ? '--' :  data.results[i].artistName}</td>
-                <td>${data.results[i].kind  === undefined ? '--' :  data.results[i].kind}</td>
-                <td>${data.results[i].trackName  === undefined ? '--' :  data.results[i].trackName}</td>
-                <td>${data.results[i].collectionName  === undefined ? '--' :  data.results[i].collectionName}</td>
-                <td>${data.results[i].primaryGenreName  === undefined ? '--' :  data.results[i].primaryGenreName}</td>
-                <td>${data.results[i].shortDescription === undefined ? '--' :  data.results[i].shortDescription + '...'}</td>
-      
-            
-
-</tr>
-                                     
-     
-`);
+                    $('#dataTable').append(`<tr><th scope="row">${num++}</th>
+                                                <td>${data.results[i].artworkUrl60  === undefined ? '--' :  '<img alt="album art" src='+ data.results[i].artworkUrl60 + '/>'}</td>
+                                                <td>${data.results[i].artistName  === undefined ? '--' :  data.results[i].artistName}</td>
+                                                <td>${data.results[i].kind  === undefined ? '--' :  data.results[i].kind}</td>
+                                                <td>${data.results[i].trackName  === undefined ? '--' :  data.results[i].trackName}</td>
+                                                <td>${data.results[i].collectionName  === undefined ? '--' :  data.results[i].collectionName}</td>
+                                                <td>${data.results[i].primaryGenreName  === undefined ? '--' :  data.results[i].primaryGenreName}</td>
+                                                <td>${data.results[i].shortDescription === undefined ? '--' :  data.results[i].shortDescription + '...'}</td>
+                                            </tr>`);
                 }
             },
             'json'
