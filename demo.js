4ee279ae27db329f79d605262ca3fa5a9a20f88e
Andrew <andrewmgunn26@gmail.com>
2022-03-29 20:56:59 -0500

    artwork added

--- demo.js
+++ demo.js
@@ -31,12 +31,15 @@ $(function () {
                         `
 <tr>
 <th scope="row">${num++}</th>
-                <td>${data.results[i].artistName}</td>
-                <td>${data.results[i].kind}</td>
-                <td>${data.results[i].trackName}</td>
+                <td><img src="${data.results[i].artworkUrl60}" /></td>
+
+
+                <td>${data.results[i].artistName  === undefined ? '--' :  data.results[i].artistName}</td>
+                <td>${data.results[i].kind  === undefined ? '--' :  data.results[i].kind}</td>
+                <td>${data.results[i].trackName  === undefined ? '--' :  data.results[i].trackName}</td>
                 <td>${data.results[i].collectionName  === undefined ? '--' :  data.results[i].collectionName}</td>
-                <td>${data.results[i].primaryGenreName}</td>
-                <td>${data.results[i].shortDescription === undefined ? '--' :  data.results[i].shortDescription}</td>
+                <td>${data.results[i].primaryGenreName  === undefined ? '--' :  data.results[i].primaryGenreName}</td>
+                <td>${data.results[i].shortDescription === undefined ? '--' :  data.results[i].shortDescription + '...'}</td>
       
             
 
